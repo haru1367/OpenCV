56283c1860596cd26cd1e583609be68f76b8b079
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 12:27:50 +0900

    Create main.cpp

---
+++ study/Chapter7/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+
+extern void affine_transform();
+extern void rotate_flip();
+extern void perspective();
+using namespace std;
+
+int main() {
+	//affine_transform();
+	//rotate_flip();
+	perspective();
+}
