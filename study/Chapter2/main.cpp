892622d1b075cad3c408ef7e85ec3d488318e758
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 12:22:41 +0900

    Create main.cpp

---
+++ study/Chapter2/main.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+
+extern void show_images();
+extern void show_code_3();
+extern void show_code_3_8();
+extern void show_code_3_9();
+extern void show_code_3_10();
+extern void show_code_3_11();
+extern void show_code_3_12();
+extern void show_code_3_13();
+extern void show_code_3_16();
+
+using namespace std;
+
+int main() {
+    //cout << CV_VERSION << endl;
+
+    //show_images();
+    //show_code_3();
+    //show_code_3_8();
+    //show_code_3_9();
+    //show_code_3_10();
+    //show_code_3_11();
+    //show_code_3_12();
+    //show_code_3_13();
+    show_code_3_16();
+}
