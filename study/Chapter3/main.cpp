675d65141f02e98a542e588b9b36ed64b16c670b
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 12:24:22 +0900

    Create main.cpp

---
+++ study/Chapter3/main.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/opencv.hpp>
+
+extern void show_images();
+extern void show_code_3();
+extern void show_code_3_8();
+extern void show_code_3_9();
+extern void show_code_3_10();
+extern void show_code_3_11();
+extern void show_code_3_12();
+extern void show_code_3_13();
+extern void show_code_3_16();
+extern void show_camera();
+extern void show_movie();
+extern void show_video_record();
+extern void show_draw_lines();
+extern void show_draw_polygons();
+extern void show_draw_text();
+extern void keyboard_event();
+extern void mouse_event();
+extern void trackbar_event();
+extern void mask_setTo();
+extern void mask_copyTo();
+extern void time_inverse();
+extern void useful_function();
+
+using namespace std;
+
+int main() {
+    //cout << CV_VERSION << endl;
+
+    //show_images();
+    //show_code_3();
+    //show_code_3_8();
+    //show_code_3_9();
+    //show_code_3_10();
+    //show_code_3_11();
+    //show_code_3_12();
+    //show_code_3_13();
+    //show_code_3_16();
+    //show_camera();
+    //show_movie();
+    //show_video_record();
+    //show_draw_lines();
+    //show_draw_polygons();
+    //show_draw_text();
+    //keyboard_event();
+    //mouse_event();
+    //trackbar_event();
+    //mask_setTo();
+    //mask_copyTo();
+    //time_inverse();
+    useful_function();
+}
