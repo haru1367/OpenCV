63c36974a48747f35e17abd0cd47ae5c178a76ac
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 12:25:08 +0900

    Create main.cpp

---
+++ study/Chapter4/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+
+extern void example();
+extern void brightness();
+extern void brightness4();
+extern void contrast();
+extern void show_hist();
+extern void histogram_streching();
+
+using namespace std;
+
+int main() {
+    //example();
+    //brightness();
+    //brightness4();
+    //contrast();
+    //show_hist();
+    histogram_streching();
+}
