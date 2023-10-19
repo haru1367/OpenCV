ab527fdfcb65a090c9c70031f4a572f5250660d5
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 12:31:58 +0900

    Create main.cpp

---
+++ study/Chapter10/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+extern void using_threshold();
+extern void adaptive_threshold();
+extern void erode_dilate();
+extern void open_close();
+
+int main() {
+	//using_threshold();
+	//adaptive_threshold();
+	//erode_dilate();
+	open_close();
+}
